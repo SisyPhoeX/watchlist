f72f929a84e88d8fceb0be140dc0f9e9569dabe3
SisyPhoeX <yiming980421@yeah.net>
2020-02-13 23:49:00 +0800

    Add database

--- app.py
+++ app.py
@@ -1,11 +1,64 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+import click
 
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60), unique=True)
+    year = db.Column(db.String(4))
+
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = 'Yiming Li'
+    movies = [
+        {'title': 'abc', 'year': '2049'},
+        {'title': 'My life', 'year': '1777'},
+        {'title': 'Der Untergang', 'year': '2004'}
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
 
 
 if __name__ == '__main__':
--- readme.txt
+++ readme.txt
@@ -2,5 +2,8 @@
 关于一个python项目的虚拟环境：
 尽管用pycharm创建新工程时虚拟已经自动建好了，但是由于有一些包无法通过pycharm来安装，只好在命令行里用
 pip install package 来安装。此时就需要命令行处在已经激活的project/Scripts/env下
-然而我的电脑在gitbash下由于未知原因无法激活虚拟环境，因此换用conda prompt
+$ env\Scripts\activate
+然而我的电脑在gitbash下由于未知原因无法激活虚拟环境，因此换用anaconda prompt
 ——————————————————————————————————
+关于CSS样式：
+Chrome浏览器会缓存CSS样式，所以每次测试时需按Ctrl+F5清除缓存
